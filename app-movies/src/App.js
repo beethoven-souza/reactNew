a01b732d2065c219abd29917c8be05506fb064bd
beethoven.souza-at-117143991034 <beethoven.souza@cyberxpro.com.br>
2025-04-16 17:57:06 -0300

    initial commit

---
+++ app-movies/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Index";
+import Footer from "./components/Footer/Index";
+import Banner from "./components/Banner/Index";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Banner></Banner>
+      <h1>Hello World!!</h1>
+      <p>Olá mundo! Estou prendendo React.</p>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ app-movies/src/components/Footer/Index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h2>MasterFlix &copy; Desenvolvido por Empresas Master 2025</h2>
+    </footer>
+  );
+}
+
+export default Footer;
