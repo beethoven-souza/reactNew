86a8c376ae41cf12e2b142ce428b65b130b451fd
Beethoven Souza <68762246+beethoven-souza@users.noreply.github.com>
2025-04-16 22:57:02 -0300

    component Banner

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -1,14 +1,19 @@
 import Header from "./components/Header/Index";
 import Footer from "./components/Footer/Index";
 import Banner from "./components/Banner/Index";
+import Container from "./components/Container/Index";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Banner></Banner>
+      <Banner image="home"/>
+
+      <Container>
       <h1>Hello World!!</h1>
       <p>Olá mundo! Estou prendendo React.</p>
+      </Container>
+
       <Footer></Footer>
     </>
   );
--- app-movies/src/components/Banner/Index.js
+++ app-movies/src/components/Banner/Index.js
@@ -1,5 +1,14 @@
 import styles from "./Banner.module.css";
 
-function Banner() {
-  return <div className={styles.banner}></div>;
+function Banner({image}) {
+  return(
+    <div 
+      className={styles.banner}
+      style={{ backgroundImage:`url('/images/banner-${image}.png')`}}
+      >
+    
+    </div>
+  );
 }
+
+export default Banner;
